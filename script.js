64d3690369edba86c26d117ab449ba0a022dbe14
nilsreichardt <nils@reichardt.io>
2022-05-27 23:24:32 +0200

    Add comments and refactor

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// The HTML element looks like "<span class="mail-status unread "
+// data-target="notification-indicator.modifier"></span>". We search by the
+// class names.
 const matches = document.getElementsByClassName('mail-status unread');
-if (matches.length != 0) {
-    for (const element of matches) {
-        element.remove();
-    }
+for (const element of matches) {
+    // Removing the unread notifications indicator from the DOM.
+    element.remove();
 }
